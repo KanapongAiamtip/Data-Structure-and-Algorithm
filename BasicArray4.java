ac7c2029565ad7856adc52e455fb0074cda361b2
KanapongAiamtip <kannaphongi62@nu.ac.th>
2021-06-28 01:28:05 +0700

    Basic Array P4

--- BasicArray4.java
+++ BasicArray4.java
@@ -4,13 +4,11 @@ public class BasicArray4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int test[] = new int[10];
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < test.length; i++) {
             test[i] = sc.nextInt();
         }
         for (int i = 0; i < test.length; i++) {
-
             System.out.print(test[i]);
-
         }
     }
 }
