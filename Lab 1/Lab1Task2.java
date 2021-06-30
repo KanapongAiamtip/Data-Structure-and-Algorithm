6314feb73c78a8406dda99e515b32534f84bc572
KanapongAiamtip <kannaphongi62@nu.ac.th>
2021-06-30 12:21:46 +0700

    Lab1

---
+++ Lab 1/Lab1Task2.java
@@ -0,0 +1,9 @@
+public class Lab1Task2 {
+    public static void main(String[] args) {
+        int arr[] = { 2, 4, 5, 87, 15, 24, 32 };
+        for (int i : arr) {
+            System.err.print(i + " ");
+        }
+
+    }
+}
