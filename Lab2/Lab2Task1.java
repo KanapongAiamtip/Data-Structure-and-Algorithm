d9e38605926486503997dfda3d64155eab681448
KanapongAiamtip <kannaphongi62@nu.ac.th>
2021-07-07 10:58:00 +0700

    Lab2

---
+++ Lab2/Lab2Task1.java
@@ -0,0 +1,8 @@
+public class Lab2Task1 {
+    public static void main(String[] args) {
+        int x[][] = { { 10, 12, 50 }, { 45, 98, 32, 2 }, { 78, 10 }, { 48, 45, 80 } };
+        System.out.println(x.length);
+        System.out.println(x[1].length);
+        System.out.println(x[3][2]);
+    }
+}
