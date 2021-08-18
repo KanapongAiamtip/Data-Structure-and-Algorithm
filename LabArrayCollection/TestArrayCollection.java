efb32c1cb7a295fb3c1b84da4acb7c5bc1d2103a
Louis Thanawin <kannaphongi62@nu.ac.th>
2021-08-19 03:27:05 +0700

    Lab ArrayCollection

---
+++ LabArrayCollection/TestArrayCollection.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class TestArrayCollection {
+    /*public static void main(String[] args) { //Task2
+        ArrayCollection mydata = new ArrayCollection(10);
+        System.out.println(mydata.size());
+    }*/
+    
+    /*public static void main(String[] args) {
+        ArrayCollection mydata = new ArrayCollection(10); //Task3
+        Object d = input();
+        mydata.add(d);
+        //for (int i = 1; i <= 4; i++) {mydata.add(input());}
+        System.out.println(mydata.size());
+    }
+    private static Object input() {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        return s;
+    }*/
+
+    /*public static void main(String[] args) { //TaskFinish
+        ArrayCollection mydata = new ArrayCollection(10);
+        // System.out.println(mydata.size());
+        // Object data = input();
+        // mydata.add(data);
+        for (int i = 1; i <= 5; i++) {
+            mydata.add(input());
+        }
+        boolean b = mydata.contains(input());
+        System.out.println(b);
+        System.out.println(mydata.search(input()));
+        mydata.remove(input());
+        System.out.println(mydata.size());
+        System.out.println(mydata);
+    }
+    private static Object input() {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        return s;
+    }*/
+}
