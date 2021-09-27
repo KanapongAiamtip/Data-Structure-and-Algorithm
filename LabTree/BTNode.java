b4435f6b12a6cf23e0df76299e417ed83f9951dc
Louis Thanawin <kannaphongi62@nu.ac.th>
2021-09-28 03:47:25 +0700

    Lab Tree

---
+++ LabTree/BTNode.java
@@ -0,0 +1,16 @@
+public class BTNode{
+    public BTNode left , right ;
+    public int data;
+    
+    public BTNode(){
+        left = null;
+        right = null;
+        data = 0;
+    }
+
+    public BTNode(int data){
+        left = null;
+        right = null;
+        this.data = data;
+    }
+}
